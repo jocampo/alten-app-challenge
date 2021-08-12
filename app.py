ffd1fe974909fe09a0163a4ea974ef5215c56d89
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-12 18:24:55 -0500

    renames and connection to db, still a WIP

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import os
 from flask import Flask
+from flask_cors import CORS
+from flask_heroku import Heroku
+from sqlalchemy import create_engine
+
+from config import DATABASE_URL
+os.environ["DATABASE_URL"] = DATABASE_URL
 
 app = Flask(__name__)
+heroku = Heroku(app)
+CORS(app, support_credentials=True)
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
 
 @app.route('/')
@@ -8,5 +19,5 @@ def hello_world():
     return 'Hello World!'
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- config/__init__.py
+++ config/__init__.py
@@ -1,15 +1,11 @@
-import os
 import subprocess
 
 # Get the latest Database URL (which heroku cycles periodically) from the Heroku CLI
-# in environments that can't invoke the .envrc file contents
 DATABASE_URL = subprocess.Popen("heroku config:get DATABASE_URL -a jocampo-alten-app-challenge",
                                 shell=True,
                                 stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE).communicate()[0]
+                                stderr=subprocess.PIPE).communicate()[0].decode("utf-8")
 
-os.environ["DATABASE_URL"] = DATABASE_URL.decode("utf-8")
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+# Fix an issue with the now deprecated postgres dialect that Heroku gives back
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
