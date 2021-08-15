75caad5b8af723b1f862d477a1457cfbc9177f36
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-15 15:49:43 -0500

    First draft of error messages

---
+++ business/entities/ErrorMessages.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class ErrorMessages(Enum):
+    """
+    Enum class to keep the known error messages in a single place
+    """
+
+    """
+    Generic Internal Server Error Message
+    """
+    INTERNAL_SERVER_ERROR_MESSAGE = 'An error occurred while trying to perform the operation.'
+
