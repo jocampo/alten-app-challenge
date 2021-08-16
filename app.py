433300a9467efdce24440e0b970878c74534721e
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-15 23:21:49 -0500

    First decent pass at operational app, needs some cleanup and for configs to be moved elsewhere. WIP

--- app.py
+++ app.py
@@ -2,25 +2,24 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_heroku import Heroku
+from flask_restful import Api
 from flask_sqlalchemy_session import flask_scoped_session
-from sqlalchemy import create_engine
 
+from api.controllers.GuestBaseController import GuestBaseController
+from api.controllers.GuestController import GuestController
+from api.controllers.Routes import Routes
 from config import DATABASE_URL
-from db.AbstractDAO import AbstractDAO
 from db.ConnectionManager import ConnectionManager
-from db.GuestDAO import GuestDAO
-from db.entities.Guest import Guest
-from db.entities.Room import Room
 from utils.heroku import HerokuUtils
 
 if "DATABASE_URL" in os.environ:
-    db_url = HerokuUtils.parse_postgres_dialect(os.environ['DATABASE_URL'])
-    # os.environ['DATABASE_URL'] = db_url
+    db_url = HerokuUtils.parse_postgres_dialect(os.environ["DATABASE_URL"])
 else:
     os.environ["DATABASE_URL"] = DATABASE_URL
     db_url = DATABASE_URL
 
 app = Flask(__name__)
+api = Api(app)
 heroku = Heroku(app)
 CORS(app, support_credentials=True)
 
@@ -28,14 +27,13 @@ connection_manager = ConnectionManager(db_url)
 session = flask_scoped_session(connection_manager.get_session_factory(), app)
 
 
-@app.route('/')
-def hello_world():
-    guest = Guest("222222", "Pepe", "Suarez")
-    GuestDAO.begin()
-    GuestDAO.save(guest)
-    GuestDAO.commit()
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return "Welcome to the Hotel API"
 
+
+api.add_resource(GuestBaseController, Routes.GUESTS.value)
+api.add_resource(GuestController, Routes.GUESTS_QUERY_PARAMS.value)
 
 if __name__ == "__main__":
     app.run()
