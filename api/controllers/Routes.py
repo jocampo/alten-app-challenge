149e5bbdf1aa8435984c7746cdab6b1832231635
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-16 02:21:05 -0500

    INTSSS

--- api/controllers/Routes.py
+++ api/controllers/Routes.py
@@ -12,16 +12,16 @@ class Routes(Enum):
     GUESTS = f"{API_BASE}/guests"
 
     """ API routes pertaining operations with the Guest entity by id """
-    GUESTS_BY_ID = f"{GUESTS}/<string:guest_id>"
+    GUESTS_BY_ID = f"{GUESTS}/<int:guest_id>"
 
     """ API routes pertaining operations with the Room entity """
     ROOMS = f"{API_BASE}/rooms"
 
     """ API routes pertaining operations with the Room entity by id """
-    ROOMS_BY_ID = f"{ROOMS}/<string:room_id>"
+    ROOMS_BY_ID = f"{ROOMS}/<int:room_id>"
 
     """ API routes pertaining operations with the Reservation entity """
     RESERVATIONS = f"{API_BASE}/reservations"
 
     """ API routes pertaining operations with the Reservation entity by id """
-    RESERVATIONS_BY_ID = f"{RESERVATIONS}/<string:reservation_id>"
+    RESERVATIONS_BY_ID = f"{RESERVATIONS}/<int:reservation_id>"
