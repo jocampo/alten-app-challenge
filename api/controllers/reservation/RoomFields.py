c081b6a29c2766d63df83ac0ba2ce0b1731a23d1
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-16 14:52:52 -0500

    Create RoomFields.py

---
+++ api/controllers/reservation/RoomFields.py
@@ -0,0 +1,53 @@
+from enum import Enum
+
+
+class RoomFields(Enum):
+    """ Collection of fields related to the room resource for validation purposes """
+
+    ID = "id"
+
+    NAME = "name"
+
+    CAPACITY = "capacity"
+
+    IS_ACTIVE = "is_active"
+
+    CREATED_AT = "created_at"
+
+    UPDATED_AT = "updated_at"
+
+
+""" Collection of allowed fields for the POST operation """
+ALLOWED_POST_FIELDS = frozenset((
+    RoomFields.NAME.value,
+    RoomFields.CAPACITY.value,
+    RoomFields.IS_ACTIVE.value
+))
+
+""" Make sure this collection makes sense """
+assert all([RoomFields(x) for x in ALLOWED_POST_FIELDS])
+
+""" Collection of required fields for the POST operation """
+REQUIRED_POST_FIELDS = frozenset((
+    RoomFields.NAME.value,
+))
+
+""" Make sure this collection makes sense """
+assert all([RoomFields(x) for x in REQUIRED_POST_FIELDS])
+
+""" Collection of allowed fields for the PUT operation """
+ALLOWED_PUT_FIELDS = frozenset((
+    RoomFields.ID.value,
+    RoomFields.NAME.value,
+    RoomFields.CAPACITY.value,
+    RoomFields.IS_ACTIVE.value
+))
+
+""" Make sure this collection makes sense """
+assert all([RoomFields(x) for x in ALLOWED_PUT_FIELDS])
+
+""" Collection of required fields for the PUT operation """
+REQUIRED_PUT_FIELDS = frozenset(())
+
+""" Make sure this collection makes sense """
+assert all([RoomFields(x) for x in REQUIRED_PUT_FIELDS])
