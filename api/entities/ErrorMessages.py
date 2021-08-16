6685a7775c8095296ed650cc9eae196070c04d88
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-15 23:17:59 -0500

    Update ErrorMessages.py

--- api/entities/ErrorMessages.py
+++ api/entities/ErrorMessages.py
@@ -6,8 +6,9 @@ class ErrorMessages(Enum):
     Enum class to keep the known error messages in a single place
     """
 
-    """
-    Generic Internal Server Error Message
-    """
+    """ Generic Internal Server Error Message """
     INTERNAL_SERVER_ERROR_MESSAGE = 'An error occurred while trying to perform the operation.'
+
+    """ Resource Not Found Error Message """
+    RESOURCE_NOT_FOUND_ERROR_MESSAGE = 'The request resource was not found.'
 
