e2172c5c6ef26412a96df2584dfcd640dbc733ab
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-17 01:43:17 -0500

    import cleanup

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
+
 from flask import Flask
 from flask_cors import CORS
 from flask_heroku import Heroku
 from flask_restful import Api
 from flask_sqlalchemy_session import flask_scoped_session
 
+from api.controllers.Routes import Routes
 from api.controllers.guest.GuestByIdController import GuestByIdController
 from api.controllers.guest.GuestController import GuestController
-from api.controllers.Routes import Routes
 from api.controllers.reservation.ReservationByIdController import ReservationByIdController
 from api.controllers.reservation.ReservationController import ReservationController
 from api.controllers.room.RoomByIdController import RoomByIdController
