6b0e12e3a77e65cc888b6033de4225f24f6c1bc0
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-19 02:50:42 -0500

    "fixed"/bypassed/circumvented weirdness around the mock patch decorator -_-

--- test/app/TestAppBase.py
+++ test/app/TestAppBase.py
@@ -5,24 +5,31 @@ from alembic.command import downgrade, upgrade
 from alembic.config import Config
 from flask_testing import TestCase
 
-from app import create_app
+from config.CreateApp import create_app
 from db.AbstractDAO import AbstractDAO
 
 
-@mock.patch.dict(os.environ, {"DATABASE_URL": "sqlite://"})
 class TestAppBase(TestCase):
     """
     Base test class to inherit from in order to run tests related to the flask app/db.
     It mocks the OS environment variable DATABASE_URL to point towards an in-memory
     sqlite database for tests.
+
+    NOTE: The mock patch decorator has been added to every method in this class instead of at the class level
+    because, if we do that, it will only apply for methods in the class that start with test_... which is annoying,
+    since we inherit from a superclass and can't really rename them.
     """
 
+    MOCK_DATABASE_URL = "sqlite://"
+
+    @mock.patch.dict("os.environ", {"DATABASE_URL": MOCK_DATABASE_URL})
     def create_app(self):
         """
         Essentially sets up the flask app
         """
         return create_app(True)
 
+    @mock.patch.dict("os.environ", {"DATABASE_URL": MOCK_DATABASE_URL})
     def setUp(self):
         """
         To be ran before all tests
@@ -31,6 +38,7 @@ class TestAppBase(TestCase):
         # Run migrations all the way to head (all of them)
         upgrade(self.__alembic_cfg, "head")
 
+    @mock.patch.dict("os.environ", {"DATABASE_URL": MOCK_DATABASE_URL})
     def tearDown(self):
         """
         Performs clean-up after every test case
