e9e13ce1b6d8709ddbd29055375690e8e3919c52
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-08-18 19:49:44 -0500

    Update TestAppBase.py

--- test/app/TestAppBase.py
+++ test/app/TestAppBase.py
@@ -11,14 +11,31 @@ from db.AbstractDAO import AbstractDAO
 
 @mock.patch.dict(os.environ, {"DATABASE_URL": "sqlite://"})
 class TestAppBase(TestCase):
+    """
+    Base test class to inherit from in order to run tests related to the flask app/db.
+    It mocks the OS environment variable DATABASE_URL to point towards an in-memory
+    sqlite database for tests.
+    """
 
     def create_app(self):
+        """
+        Essentially sets up the flask app
+        """
         return create_app(True)
 
     def setUp(self):
+        """
+        To be ran before all tests
+        """
         self.__alembic_cfg = Config("alembic.ini")
+        # Run migrations all the way to head (all of them)
         upgrade(self.__alembic_cfg, "head")
 
     def tearDown(self):
+        """
+        Performs clean-up after every test case
+        """
+        # Remove the existing session
         AbstractDAO.get_connection().remove()
+        # Cleanup all the db objects
         downgrade(self.__alembic_cfg, "base")
